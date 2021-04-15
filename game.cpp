0656506eb6bdfe98e28ecb454a465487c43d0a34
Ryo Hilmi <ryodm.74@gmail.com>
2021-04-15 11:53:31 +0700

    Add move cursor function

--- game.cpp
+++ game.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-void printBoard(char board[3][3])
+void printBoard(char board[3][3], int x, int y)
 {
   cout << "-------------" << endl;
 
@@ -14,7 +14,10 @@ void printBoard(char board[3][3])
   {
     for (int j = 0; j < 3; j++)
     {
-      char block = board[i][j];
+      char block = (i == y && j == x)
+                       ? '_'
+                       : board[i][j];
+
       cout << "| " << block << " ";
     }
 
@@ -22,6 +25,50 @@ void printBoard(char board[3][3])
   }
 
   cout << "-------------" << endl;
+}
+
+void moveCursor(int *x, int *y, char dir)
+{
+  if (dir == 'U')
+  {
+    *y = (*y - 1 < 0) ? 2 : *y - 1;
+  }
+  else if (dir == 'D')
+  {
+    *y = (*y + 1 > 2) ? 0 : *y + 1;
+  }
+  else if (dir == 'L')
+  {
+    *x = (*x - 1 < 0) ? 2 : *x - 1;
+  }
+  else if (dir == 'R')
+  {
+    *x = (*x + 1 > 2) ? 0 : *x + 1;
+  }
+}
+
+char changeDirection(char inputChar)
+{
+  if (inputChar == 'w')
+  {
+    return 'U';
+  }
+  else if (inputChar == 's')
+  {
+    return 'D';
+  }
+  else if (inputChar == 'a')
+  {
+    return 'L';
+  }
+  else if (inputChar == 'd')
+  {
+    return 'R';
+  }
+  else
+  {
+    return ' ';
+  }
 }
 
 int main()
@@ -35,6 +82,18 @@ int main()
   while (1)
   {
     system("cls");
-    printBoard(board);
+    printBoard(board, currentX, currentY);
+
+    if (kbhit())
+    {
+      system("cls");
+      char inputChar = getch();
+
+      direction = changeDirection(inputChar);
+
+      moveCursor(&currentX, &currentY, direction);
+
+      printBoard(board, currentX, currentY);
+    }
   }
 }
