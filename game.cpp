1ed4de8206e895765f5f0347cbc5295b68e31801
Ryo Hilmi <ryodm.74@gmail.com>
2021-04-16 16:35:54 +0700

    Finish the game

--- game.cpp
+++ game.cpp
@@ -1,17 +1,37 @@
-#include <iostream>
-#include <ctime>
-#include <stdbool.h>
-#include <windows.h>
-#include <conio.h>
-
 #include "include/tictactoe.hpp"
+#include "include/player.hpp"
+
+#include <iostream>
+#include <conio.h>
 
 using namespace std;
 
+string inputName(int playerNum)
+{
+  string name;
+  cout << "Enter player " << playerNum << "'s name : ";
+  cin >> name;
+  system("cls");
+
+  return name;
+}
+
+void printScore(Player player1, Player player2)
+{
+  cout << "Score" << endl;
+  cout << "==================" << endl;
+  cout << player1.playerName << "\t:\t" << player1.totalWin << endl;
+  cout << player2.playerName << "\t:\t" << player2.totalWin << endl;
+  cout << "==================\n\n";
+}
+
 int main()
 {
-  char winner;
+  char winnerChar;
   TicTacToe game;
+
+  Player player1(inputName(1));
+  Player player2(inputName(2));
 
   while (1)
   {
@@ -19,16 +39,38 @@ int main()
     {
       if (game.isDraw)
       {
-        cout << endl
-             << "!!!!! DRAW !!!!!" << endl;
+        cout << "\n!!!!! DRAW !!!!!\n\n";
+
+        printScore(player1, player2);
       }
       else
       {
-        cout << endl
-             << "!!!!! " << winner << " WINS !!!!!" << endl;
+        Player *winner = winnerChar == 'X' ? &player1 : &player2;
+        winner->win();
+
+        cout << "\n!!!!! " << winner->playerName << " WINS !!!!!\n\n";
+
+        printScore(player1, player2);
       }
 
-      break;
+      bool end = false;
+      char again;
+
+      cout << "Play again? (y / n) ";
+      cin >> again;
+      end = again == 'y' ? false : true;
+
+      if (end)
+      {
+        system("cls");
+        printScore(player1, player2);
+        break;
+      }
+      else
+      {
+        game.resetGame();
+        continue;
+      }
     }
 
     game.blink();
@@ -40,7 +82,7 @@ int main()
       char inputChar = getch();
 
       if (inputChar == 13)
-        game.turn(&winner);
+        game.turn(&winnerChar);
 
       game.changeDirection(inputChar);
       game.moveCursor();
